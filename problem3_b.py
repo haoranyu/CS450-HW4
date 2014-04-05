4f13c5f8efe030643433cc5ac95829b8ed1b7c35
haoranyu <haoran_yu@hotmail.com>
2014-04-05 18:57:34 -0500

    Update homework

--- problem3_b.py
+++ problem3_b.py
@@ -8,24 +8,42 @@ import matplotlib.pyplot as plt
 t = np.array([0.0, 0.25, 0.5, 0.75, 1.00, 1.25, 1.5, 1.75, 2.0])
 y = np.array([20.0, 51.58, 68.73, 75.46, 74.36, 67.09, 54.73, 37.98, 17.28])
 
-def gaussNewton(f, Jac, Res, Nx, x, t, y):
-	tol = 10e-14
+def plot_init():
+    plt.clf()
+    plt.xlabel("t")
+    plt.ylabel("f")
+    plt.title("Plot for Problem3 b) ")
+    plt.hold(True)
+
+def plot_draw(t, y, x):
+	plt.scatter(t, y)
+	t_sample = np.linspace(0, 2, 256)
+	y_sample = model(x, t_sample)
+	plt.plot(t_sample, model(x, t_sample))
+
+
+def gaussNewton(f, Jac, Res, x, t, y):
+	tol = 1e-14
 	i = 0
 	error = 1
 	while error > tol:
 		J = Jac(x, t)
 		r = Res(x, t, y)
-		error = la.norm(r)
-		print r
-		print error
+		if i > 0:
+			error = la.norm(r - r_prev)
+		r_prev = r
 		s = la.lstsq(np.copy(J), np.copy(-r))[0]
-		x = Nx(x, s)
 		if i > 500:
 			print "Error: Too many iterations"
 			break
 		else:
 			i += 1
-	return x
+
+		x = x + s
+		if(i <= 5):
+			print "x %d is" % i
+			print x
+	return x, (i-1)
 
 def model(x, t):
     return x[0] + x[1]*t + x[2]*(t**2) + x[3]*np.exp(x[4]*t)
@@ -42,8 +60,23 @@ def jac(x, t):
     ans[:,4] = x[3]*t*np.exp(x[4]*t)
     return np.copy(ans)
 
-def next_x(x, s):
-	return x+s
+x0_1 = np.array([0, 0, 0, 0, 1])
+x0_2 = np.array([1, 0, 0, 0, 0])
+x0_3 = np.array([1, 0, 0, 1, 0])
 
-x0 = np.array([1, 0, 0, 1, 0])
-x = gaussNewton(model, jac, residual, next_x, np.copy(x0), np.copy(t), np.copy(y) )
+x, i = gaussNewton(model, jac, residual, np.copy(x0_1), np.copy(t), np.copy(y) )
+plot_init()
+plot_draw(t, y, x)
+plt.savefig("problem3_b_1.png")
+print "=============================="
+
+x, i = gaussNewton(model, jac, residual, np.copy(x0_2), np.copy(t), np.copy(y) )
+plot_init()
+plot_draw(t, y, x)
+plt.savefig("problem3_b_2.png")
+print "=============================="
+
+x, i = gaussNewton(model, jac, residual, np.copy(x0_3), np.copy(t), np.copy(y) )
+plot_init()
+plot_draw(t, y, x)
+plt.savefig("problem3_b_3.png")
